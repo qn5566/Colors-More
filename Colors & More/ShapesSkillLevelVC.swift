a1fd0acb9624c62f711cd24fa55a4a74b2b8e93a
Josh Kinney <joshkinney@kinneykare.com>
2019-12-21 23:51:10 -0600

    Added more UI to this app.

---
+++ Colors & More/ShapesSkillLevelVC.swift
@@ -0,0 +1,38 @@
+//
+//  ShapesSkillLevelVC.swift
+//  Colors & More
+//
+//  Created by Kinney Kare on 12/21/19.
+//  Copyright © 2019 Kinney Kare. All rights reserved.
+//
+
+import UIKit
+
+class ShapesSkillLevelVC: UIViewController {
+
+    @IBOutlet weak var skillOneButton: UIButton!
+    @IBOutlet weak var skillTwoButton: UIButton!
+    @IBOutlet weak var skillThreeButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func skillOneButtonTapped(_ sender: Any) {
+        
+        performSegue(withIdentifier: "shapes", sender: self)
+        
+    }
+    
+    @IBAction func skillTwoButtonTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func skillThreeButtonTapped(_ sender: Any) {
+    }
+    
+}
