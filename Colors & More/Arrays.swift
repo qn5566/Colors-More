89f162ac11bc94060c49e7a1c071d4125d24c787
Josh Kinney <joshkinney@kinneykare.com>
2019-12-22 11:07:50 -0600

    setting up the model

---
+++ Colors & More/Arrays.swift
@@ -0,0 +1,19 @@
+//
+//  Arrays.swift
+//  Colors & More
+//
+//  Created by Kinney Kare on 12/22/19.
+//  Copyright © 2019 Kinney Kare. All rights reserved.
+//
+
+import Foundation
+
+
+struct ArrayOf {
+    
+    var arrayOfShapes = ["Square", "Oval", "Circle", "Octagon", "Triangle", "Star", "Plus Sign", "Rectangle", "Pentagon"]
+    
+    var arrayOfColors = ["Black", "Red", "Yellow", "Green", "Pink", "Blue", "Purple", "Grey", "Orange"]
+    
+    var arrayOfShapesAndColors = ["Black Pentagon", "Red Triangle", "Yellow Circle", "Green Oval", "Pink Star", "Blue Square", "Purple Plus Sign", "Grey Rectangle", "Orange Octagon"]
+}
--- Colors & More/ShapesGameVC.swift
+++ Colors & More/ShapesGameVC.swift
@@ -10,21 +10,53 @@ import UIKit
 
 class ShapesGameVC: UIViewController {
 
+    @IBOutlet weak var titleOfPage: UILabel!
+    @IBOutlet weak var squareImage: UIButton!
+    @IBOutlet weak var ovalImage: UIButton!
+    @IBOutlet weak var rectangleImage: UIButton!
+    @IBOutlet weak var octagonImage: UIButton!
+    @IBOutlet weak var plusSignImage: UIButton!
+    @IBOutlet weak var starImage: UIButton!
+    @IBOutlet weak var pentagonImage: UIButton!
+    @IBOutlet weak var triangleImage: UIButton!
+    @IBOutlet weak var circleImage: UIButton!
+    
+    @IBOutlet weak var numberOfScore: UILabel!
+   
+    @IBOutlet weak var strikeOne: UIButton!
+    @IBOutlet weak var strikeTwo: UIButton!
+    @IBOutlet weak var strikeThree: UIButton!
+    
+    var theshapes = [UIButton]()
+    var shuffleArray = [UIButton]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+      
+        hideStrikes()
+        
+        theshapes = [squareImage, ovalImage, rectangleImage, octagonImage, plusSignImage, starImage, pentagonImage, triangleImage, circleImage]
+        
+        shuffled()
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//this hides all three strink images
+    func hideStrikes() {
+        strikeOne.isHidden = true
+    strikeTwo.isHidden = true
+        strikeThree.isHidden = true
     }
-    */
+    
+//Shuffle the shapes
+    func shuffled() {
+        
+        for _ in 0..<theshapes.count {
+            let rand = Int(arc4random_uniform(UInt32(theshapes.count)))
+            shuffleArray.append(theshapes[rand])
+        }
+        
+    }
 
 }
